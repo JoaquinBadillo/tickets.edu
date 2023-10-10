725170ebc160764c1f2c7ff700f5596a2153ca07
pKYZ <rayojef@live.com.mx>
2023-10-10 17:43:54 -0600

    Minor changes

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -1,20 +1,26 @@
 import { Dashboard } from "./dashboard";
 import { ReportDashboard } from "./reports";
 import { UserList, UserEdit, UserCreate, UserShow } from "../components/users";
-import { TicketList, TicketEdit, TicketCreate } from "../components/tickets";
+import {
+  TicketList,
+  TicketEdit,
+  TicketCreate,
+  TicketShow,
+} from "../components/tickets";
 import { AlbumList } from "./albums";
 import LoginPage from "./login";
 
-export { 
-    AlbumList, 
-    Dashboard, 
-    LoginPage,
-    ReportDashboard,
-    TicketList, 
-    TicketEdit, 
-    TicketCreate,
-    UserList,
-    UserEdit,
-    UserCreate,
-    UserShow
+export {
+  AlbumList,
+  Dashboard,
+  LoginPage,
+  ReportDashboard,
+  TicketList,
+  TicketEdit,
+  TicketCreate,
+  TicketShow,
+  UserList,
+  UserEdit,
+  UserCreate,
+  UserShow,
 };
