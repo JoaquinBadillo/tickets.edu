5a8aba6f9d60a380a0bf441480f069218dac28fb
Pboliopra <a01782428@tec.mx>
2023-10-11 20:09:53 -0600

    Andamos tirando barras por aki

---
+++ src/components/reports/charts/incidents.tsx
@@ -0,0 +1,122 @@
+import { 
+    Cell,  
+    Bar, 
+    BarChart,
+    ResponsiveContainer,
+    Tooltip,
+    XAxis,
+    YAxis
+} from "recharts";
+
+import { TooltipProps } from "recharts";
+
+import {
+  ValueType,
+  NameType,
+} from "recharts/types/component/DefaultTooltipContent";
+
+import { TicketIncidentCount } from "./hooks/barChart";
+import { Loading } from "react-admin";
+import { useMediaQuery, Theme} from "@mui/material";
+
+import { Box } from "@mui/system";
+
+import "../styles.css";
+
+const COLORS = ["#ff5400", "#ff8e00", "#ffd200", "#81e650", "#00d267", "#00c0ff", "#8b48fe", "#ca41fc", "#ee82ee"];
+
+export default function TicketBarChart() {
+  const isSmall = useMediaQuery<Theme>((theme) => theme.breakpoints.down("sm"));
+  const res = TicketIncidentCount("reports/incidents");
+  console.log(res)
+
+  if (res.loading) return <Loading />;
+  if (res.error || res.data == null) return <p>ERROR</p>;
+
+  return (
+    <Box className="chart-container">
+      <h2 className='chart-title'>Tickets por Incidente</h2>
+      {
+        isSmall ?
+          <TicketIncidentSmall data={res.data}/> :
+          <TicketIncident data={res.data}/> 
+      }
+    </Box>
+  );
+}
+
+interface IReport {
+  name: string;
+  value: number;
+}
+
+const TicketIncident = ({data}: {data: IReport[]}) => (
+    <ResponsiveContainer width='90%' minWidth={400} height={300}>
+        <BarChart 
+          width={400} 
+          height={200} 
+          data={data} 
+          margin={{left: 0, right: 0, top: 20, bottom: 40}}
+        >
+          <XAxis dataKey="name" />
+          <YAxis />
+          <Tooltip content={<CustomTooltip />}/>
+          <Bar dataKey="value">
+          { 
+              data.map((_, index) => (
+                <Cell 
+                  key={`cell-${index}`} 
+                  fill={COLORS[index % COLORS.length]} 
+                />
+              ))
+            }
+          </Bar>
+        </BarChart>
+    </ResponsiveContainer>
+);
+
+const TicketIncidentSmall = ({data}: {data: IReport[]}) => (
+  <ResponsiveContainer width='99%' height={300}>
+      <BarChart 
+          width={300}
+          height={200} 
+          data={data} 
+          margin={{left: 0, right: 0, top: 20, bottom: 40}}
+        >
+          <XAxis dataKey="name" />
+          <YAxis />
+          <Tooltip content={<CustomTooltip />}/>
+          <Bar dataKey="value" fill="#F48686">
+          { 
+              data.map((_, index) => (
+                <Cell 
+                  key={`cell-${index}`} 
+                  fill={COLORS[index % COLORS.length]} 
+                />
+              ))
+            }
+          </Bar>
+        </BarChart>
+  </ResponsiveContainer>
+);
+
+const CustomTooltip = ({ active, payload} : TooltipProps<ValueType, NameType>) => {
+  const capitalize = (s: string) => (
+      s.charAt(0).toUpperCase() + s.slice(1)
+  );
+
+  const disp = (v: number, s: string) => (
+      `${v} ${v == 1 ? s : `${s}s`}`
+  );
+
+  if (active && payload && payload.length) {
+    return (
+      <div className="custom-tooltip">
+        <p className="title">{capitalize(payload[0].payload.name)}: </p>
+        <p className="value">{disp(payload[0].value as number, "ticket")}</p>
+      </div>
+    );
+  }
+  
+  return null;
+}
